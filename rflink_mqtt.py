032df601aaad2c8101e5934646d57bac5344c301
Patrik Hermansson <hermansson.patrik@gmail.com>
2024-02-11 17:20:55 +0100

    Fixed negative temperatures

--- rflink_mqtt.py
+++ rflink_mqtt.py
@@ -16,6 +16,9 @@ MQTT_HOST = '192.168.1.190'
 MQTT_PORT = 1883
 MQTT_TOPIC = 'Rflink'
 
+def clear_bit(value, bit_index):
+    return value & ~(1 << bit_index)
+
 if (len(sys.argv) != 2):
     print("Wrong number of args, try 'rflink_mqtt <port>'. Port can be like '/dev/ttyUSB0'")
     sys.exit(0)
@@ -53,45 +56,58 @@ while 1:
   # 20;05;Alecto V2;ID=0069;TEMP=0113;HUM=36;WINSP=0000;WINGS=0000;RAIN=0000;BAT=OK;
 
   try:
-    x = str(x)
-    inputdata = x.split(';')
-    if(inputdata[1] != "00"):
-      mqttdata["NAME"] = inputdata[2]
-
-      for item in inputdata:
-        entity = item.split("=")
-        #print (item + "(len:" + str(len(entity)) + ")")
-        if(len(entity)>1):
-        
-          if(entity[0] == "TEMP"):
-            tempdec = int(entity[1],16)	# Convert to decimal
-            #print ("tempdec: " + str(tempdec))	# Temp * 10
-            tempf = tempdec / float(10) # Force tempf to be a float to preserve decimals
-            sign = entity[1][:1]	# Highest bit is set when negative temperature
-            if (sign == '8'):
-  	          charsign = "-"
-            else:
-  	          charsign = ""
-            comptemp = charsign + str(tempf)
-            mqttdata[entity[0]] = comptemp
-            #print comptemp
-          else:
-            mqttdata[entity[0]] = entity[1]
-      jsondata = json.dumps(mqttdata)
-      #print (jsondata)
-      
-      print ("Send mqtt")
-
-      try:
-        full_topic = MQTT_TOPIC + "/" + mqttdata["NAME"] + "_" + mqttdata["ID"] 
-        mqtt_client = mqtt.Client()
-        #mqtt_client.username_pw_set(config.MQTT_USER, config.MQTT_PASS)
-        mqtt_client.connect(MQTT_HOST, MQTT_PORT)
-        mqtt_client.publish(full_topic, jsondata)
-      except:
-        print ("Unexpected error:", sys.exc_info()[0])
     
+    x = str(x)
+    
+    # To test:
+    #x="20;05;Alecto V2;ID=0069;TEMP=8032;HUM=36;WINSP=0000;WINGS=0000;RAIN=0000;BAT=OK;" 
+    # -5.0 = 50dec = 0032h. High bit high indicates negative, thus = 8032.  
+    #print(x);
+    inputdata = x.split(';')
+    listLength = len(inputdata)
+    if (listLength>0):
+        if(inputdata[1] != "00"):
+          mqttdata["NAME"] = inputdata[2]
+
+          for item in inputdata:
+            entity = item.split("=")
+            #print (item + "(len:" + str(len(entity)) + ")")
+            if(len(entity)>1):
+            
+              if(entity[0] == "TEMP"):
+              #if(entity[0] != ""):
+                #print(entity[1])
+                #entity[1] = "8032";  	# = -5.0C
+                sign = entity[1][:1]	# Highest bit is set when negative temperature
+                
+                if (sign == '8'):
+                    #print("Negative test")
+                    charsign = "-"
+                    hex = int(entity[1],16) - int(8000)
+                    #print (hex)
+                    dec = int(str(hex), 16) / 10
+                    #print(dec)
+                    
+                comptemp = charsign + str(dec)
+                mqttdata[entity[0]] = comptemp
+                print (comptemp)
+              else:
+                mqttdata[entity[0]] = entity[1]
+          jsondata = json.dumps(mqttdata)
+          #print (jsondata)
+          
+          print ("Send mqtt")
+
+          try:
+            full_topic = MQTT_TOPIC + "/" + mqttdata["NAME"] + "_" + mqttdata["ID"] 
+            mqtt_client = mqtt.Client()
+            #mqtt_client.username_pw_set(config.MQTT_USER, config.MQTT_PASS)
+            mqtt_client.connect(MQTT_HOST, MQTT_PORT)
+            mqtt_client.publish(full_topic, jsondata)
+          except:
+            print ("Unexpected error:", sys.exc_info()[0])
+        
   except AttributeError:
     traceback.print_exc()
   
-  #print("Done")
+  #print("Done")ssh 
